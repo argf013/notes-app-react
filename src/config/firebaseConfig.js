e268964f634e95e2b02c6420bd3fdd5567fa8a03
Muhamad Arfa <111162303+argf013@users.noreply.github.com>
2023-02-06 17:04:59 +0700

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,10 +1,11 @@
+//your api key
 const firebaseConfig = {
-  apiKey: 'AIzaSyAapmq4CaX5biYZeBS2qar5cbzEgBaKdN8',
-  authDomain: 'notes-b1523.firebaseapp.com',
-  projectId: 'notes-b1523',
-  storageBucket: 'notes-b1523.appspot.com',
-  messagingSenderId: '558716294169',
-  appId: '1:558716294169:web:38d2353847ba53800fc688',
+apiKey: '',
+authDomain: '',
+projectId: '',
+storageBucket: '',
+messagingSenderId: '',
+appId: '',
 };
 
 export default firebaseConfig;
